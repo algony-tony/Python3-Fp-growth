0b88b0bc354aeae8923b4b2ad1ca78cac5d574ee
C.A.Lee <sz110010@gmail.com>
2019-06-30 23:20:12 +0900

    Update setup.py
    Update version, fix bug

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@
 from distutils.core import setup
 
 setup(
-    name='fp-growth',
+    name='fp_growth',
     description='A pure-python implementation of the FP-growth algorithm.',
-    version='0.1.3',
+    version='0.1.4',
     author='Eric Naeseth',
     author_email='eric@naeseth.com',
     url='http://github.com/enaeseth/python-fp-growth/',
